cc8dbca20c03751500f315cc76c3e65c54d41c60
nathanmarshall <Nathan@nathans-mbp.capilanou.ca>
2013-11-05 14:07:14 -0800

    added slide down content

--- index.php
+++ index.php
@@ -7,6 +7,21 @@
 
 		<!-- Wrapper Start -->
 		<div class="content-wrapper">
+		
+		
+			<!-- An Expander -->
+			<article class="expand">
+				<header>
+					<h4>Article Title<span>+</span></h4>
+					
+				</header>
+				
+				<div class="article-body">
+					<p> Nulla facilisi. In vel sem. Morbi id urna in diam dignissim feugiat. Proin molestie tortor eu velit. Aliquam erat volutpat. Nullam ultrices, diam tempus vulputate egestas, eros pede varius leo, sed imperdiet lectus est ornare odio. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Proin consectetuer velit in dui. Phasellus wisi purus, interdum vitae, rutrum accumsan, viverra in, velit. Sed enim risus, congue non, tristique in, commodo eu, metus. Aenean tortor mi, imperdiet id, gravida eu, posuere eu, felis. Mauris sollicitudin, turpis in hendrerit sodales, lectus ipsum pellentesque ligula, sit amet scelerisque urna nibh ut arcu. Aliquam in lacus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla placerat aliquam wisi. Mauris viverra odio. Quisque fermentum pulvinar odio. Proin posuere est vitae ligula. Etiam euismod. Cras a eros.</p>
+				</div>
+			
+			</article>
+			<!-- End Expander -->
 			
 		</div>
 		<!-- Wrapper End -->
--- js/main.js
+++ js/main.js
@@ -5,15 +5,32 @@
 
 /* Slide ut menu JS */
 
-$(function(){
+	$(function(){
 	
-	$('.toggle-nav').click(function(){
-		
-		$('.header, .content-wrapper').toggleClass('slide');
+		$('.toggle-nav').click(function(){
+			$('.header, .content-wrapper').toggleClass('slide');
 		
 	});
 	
 	
+	/* Expand JS */
+	
+	$(function(){
+		
+		$('article').click(function(){
+			
+			$(this).children('div').slideToggle('fast');
+			
+			$(this).children('span').toggle(function(){
+				$(this).text('-');
+			}, function(){
+				$(this).text('+');
+				
+			});
+			
+		});
+		
+	});
 	
 	
 });
