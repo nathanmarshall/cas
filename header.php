b7abfb1d5200fd9a9e4e3fff8226b5fcc7e143cc
nathanmarshall <Nathan@nathans-mbp.capilanou.ca>
2013-11-05 16:12:54 -0800

    Some fixes

--- header.php
+++ header.php
@@ -2,6 +2,12 @@
 <section class="header">
 	<header>
 		<a href="#" class="toggle-nav" alt="Link: Menu show">=</a>
+		
+		<img src="images/logo.png" align="Image: Logo">		
 	</header>
 </section>
 <!-- Header End -->
+
+<!-- Menu Start -->
+
+
--- menu.php
+++ menu.php
@@ -1,15 +1,14 @@
-<!-- Menu Start -->
-<aside class="menu">
-	<!-- Menu List -->
-	<ul>
-		<li><a href="#" alt="Link: Industry">Industry</a></li>
-		<li><a href="#" alt="Link: Case Studies">Case Studies</a></li>
-		<li><a href="#" alt="Link: Software">Software</a></li>
-		<li><a href="#" alt="Link: Support">Support</a></li>
-		<li><a href="#" alt="Link: About">About</a></li>
-		<li><a href="#" alt="Link: Contact">Contact</a></li>
-	</ul>
+	<nav class="menu">
+		<!-- Menu List -->
+			<ul>
+				<li><a href="#" alt="Link: Industry">Industry</a></li>
+				<li><a href="#" alt="Link: Case Studies">Case Studies</a></li>
+				<li><a href="#" alt="Link: Software">Software</a></li>
+				<li><a href="#" alt="Link: Support">Support</a></li>
+				<li><a href="#" alt="Link: About">About</a></li>
+				<li><a href="#" alt="Link: Contact">Contact</a></li>
+			</ul>
 	<!-- Menu list End -->
-</aside>
-<!-- Menu End -->
-
+	</nav>
+	<!-- Menu End -->
+		
