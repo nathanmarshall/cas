efff65ceb55ac3d78a834201449c6200959c6710
Your Name Here <mitch.budreski@gmail.com>
2013-12-01 12:18:21 -0800

    New Images Uploaded

--- dropdown.php
+++ dropdown.php
@@ -8,6 +8,15 @@
 	
 	<div class="article-body">
 		<p> Nulla facilisi. In vel sem. Morbi id urna in diam dignissim feugiat. Proin molestie tortor eu velit. Aliquam erat volutpat. Nullam ultrices, diam tempus vulputate egestas, eros pede varius leo, sed imperdiet lectus est ornare odio. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Proin consectetuer velit in dui. Phasellus wisi purus, interdum vitae, rutrum accumsan, viverra in, velit. Sed enim risus, congue non, tristique in, commodo eu, metus. Aenean tortor mi, imperdiet id, gravida eu, posuere eu, felis. Mauris sollicitudin, turpis in hendrerit sodales, lectus ipsum pellentesque ligula, sit amet scelerisque urna nibh ut arcu. Aliquam in lacus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla placerat aliquam wisi. Mauris viverra odio. Quisque fermentum pulvinar odio. Proin posuere est vitae ligula. Etiam euismod. Cras a eros.</p>
+		<img src="images/foodbev/bulkfilling.jpg" alt="" width="640" height="480">
+		<p class="imagecaption">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel, veritatis, adipisci illo fuga soluta iste debitis laboriosam. Repellat.</p>
+		<img src="images/foodbev/packageinspection.jpg" alt="" width="640" height="480">
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel, veritatis, adipisci illo fuga soluta iste debitis laboriosam. Repellat.</p>
+		<img src="images/foodbev/washline.jpg" alt="" width="640px" height="480px">
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel, veritatis, adipisci illo fuga soluta iste debitis laboriosam. Repellat.</p>
+		<img src="images/foodbev/wrapper.jpg" alt="" width="640px" height="480px">
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel, veritatis, adipisci illo fuga soluta iste debitis laboriosam. Repellat.</p>
+
 	</div>
 
 </article>
