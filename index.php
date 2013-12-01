6cde333fed7d102e6772842304f1391ef5b9abcf
Your Name Here <mitch.budreski@gmail.com>
2013-12-01 10:39:51 -0800

    Front Page Completed
    Doing it!

--- index.php
+++ index.php
@@ -15,21 +15,7 @@
 		<!-- Wrapper Start -->
 		<div class="content-wrapper">
 		
-		
-			<!-- An Expander *** Copy Paste this code *** -->
-			<article class="expand">
-				<header>
-					<h4>Article Title<span class="icon-arrow-down"></span></h4>
-					
-				</header>
-				
-				<div class="article-body">
-					<p> Nulla facilisi. In vel sem. Morbi id urna in diam dignissim feugiat. Proin molestie tortor eu velit. Aliquam erat volutpat. Nullam ultrices, diam tempus vulputate egestas, eros pede varius leo, sed imperdiet lectus est ornare odio. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Proin consectetuer velit in dui. Phasellus wisi purus, interdum vitae, rutrum accumsan, viverra in, velit. Sed enim risus, congue non, tristique in, commodo eu, metus. Aenean tortor mi, imperdiet id, gravida eu, posuere eu, felis. Mauris sollicitudin, turpis in hendrerit sodales, lectus ipsum pellentesque ligula, sit amet scelerisque urna nibh ut arcu. Aliquam in lacus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla placerat aliquam wisi. Mauris viverra odio. Quisque fermentum pulvinar odio. Proin posuere est vitae ligula. Etiam euismod. Cras a eros.</p>
-				</div>
-			
-			</article>
-			<!-- End Expander -->
-			
+				<?php include_once('landingbody.php'); ?>
 			
 			
 		</div>
---
+++ landingbody.php
@@ -0,0 +1,29 @@
+
+
+<div class="mission">
+<h2 class="headlinetop">Our experienced team of food and beverage experts starts the process</h2>
+<h2 class="headlinebottom">with a complete assessment of the existing operation. </h2>
+<p>Beyond the standard approach of looking for bottlenecks, downtime and line delays; our group will determine methods to make the existing system run faster and cleaner. Continuous operation at faster speeds will significantly impact productivity. Avoid waste with improved accuracy on filling and packaging.</p>
+</div>
+
+<div class="values clearfix">
+	<img src="images/mushrooms.jpg" alt="">
+	<div class="values-content right"><h2>Productivity</h2>
+		<p>Changing business conditions and the emergence of new technologies are accelerating the need for new processes and equipment.</p>
+		<p>Manufacturers typically recommend equipment replacement or costly upgrades to improve productivity. The Confirmed Automation approach optimizes the operation of the existing equipment.</p>
+		<p>By focusing on monitoring and improved control of the equipment, we provide our customers with significant  withsignificant and sustainable productivity gains. Payback is often measured in months rather than years.
+		Our company has been created by a team of operations experts --- that's where the focus lies.</p>
+		</div>
+
+</div>
+
+
+<div class="values clearfix">
+	<div class="values-content left"><h2>Efficiency</h2>
+		<p>Changing business conditions and the emergence of new technologies are accelerating the need for new processes and equipment.</p>
+		<p>Manufacturers typically recommend equipment replacement or costly upgrades to improve productivity. The Confirmed Automation approach optimizes the operation of the existing equipment.</p>
+		<p>By focusing on monitoring and improved control of the equipment, we provide our customers with significant  withsignificant and sustainable productivity gains. Payback is often measured in months rather than years.
+		Our company has been created by a team of operations experts --- that's where the focus lies.</p>
+	</div>
+	<img src="images/coalstack.jpg" alt="">
+</div>
